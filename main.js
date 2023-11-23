8dd099409b96ff30fa1ceab3b1ea1ec1216503ad
apple714411 <94893788+apple714411@users.noreply.github.com>
2023-11-23 20:30:35 +0900

    날쒸

--- main.js
+++ main.js
@@ -1,45 +1,23 @@
-const comPic = document.querySelector(".comPic");
-const myPicR = document.querySelector(".myPicR");
-const myPicS = document.querySelector(".myPicS");
-const myPicP = document.querySelector(".myPicP");
-const forMain2 = document.querySelector(".forMain2");
+const API_KEY = "022c44fda65f777ac64bc741ad25b005";
 
-const RSP = ["묵", "찌", "빠"];
-const caseR = { 묵: "비겼다", 찌: "이겼다", 빠: "졌다" };
-const caseS = { 묵: "졌다", 찌: "비겼다", 빠: "이겼다" };
-const caseP = { 묵: "이겼다", 찌: "졌다", 빠: "비겼다" };
+// https://openweathermap.org/current
 
-function randomPic() {
-  const randomNum = Math.floor(Math.random() * 3);
-  const randomRSP = RSP[randomNum];
-  return randomRSP;
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather div:first-child");
+      const city = document.querySelector("#weather div:last-child");
+      console.log(data.weather[0].main);
+      city.innerText = `${data.name}`;
+      weather.innerText = data.weather[0].main;
+    });
+}
+function onGeoError() {
+  alert();
 }
 
-function mainFunction(pic) {
-  const randomRSP = randomPic();
-  comPic.innerText = randomRSP;
-  if (pic == "묵") {
-    const result = caseR[randomRSP];
-    forMain2.innerText = `이번판은 ${result}!`;
-  } else if (pic == "찌") {
-    const result = caseS[randomRSP];
-    forMain2.innerText = `이번판은 ${result}!`;
-  } else {
-    const result = caseP[randomRSP];
-    forMain2.innerText = `이번판은 ${result}!`;
-  }
-}
-
-myPicR.addEventListener("click", function () {
-  mainFunction("묵");
-});
-myPicS.addEventListener("click", function () {
-  mainFunction("찌");
-});
-myPicP.addEventListener("click", function () {
-  mainFunction("빠");
-});
-
-// 1. 메인함수로 내가 선택한 묵찌빠를 보낸다
-// 2. 랜덤RSP로 퓨터가 선택한 묵찌빠를 가져온다
-// 3. if문을 통해서 승패결과를 가른다
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
